bbd152ada7035e4e3164da400da1671fabeb417e
burritoslut <kingburrito@burritoslut.com>
2023-07-08 13:09:16 -0700

    Re-Creating Heroku Apps with correct routing

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
-//api.js
+// api.js
 
 import axios from 'axios';
+import { SERVER_URL } from './config';
 
 const instance = axios.create({
-    baseURL: 'https://protected-escarpment-93524-e347b697cce9.herokuapp.com'
+    baseURL: SERVER_URL
 });
 
 export default instance;
---
+++ src/config.js
@@ -0,0 +1,3 @@
+// config.js
+
+export const SERVER_URL = 'https://burritoserver-fcf8b441139d.herokuapp.com';
