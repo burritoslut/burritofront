20b4eff3a337215699c0dba29389e0bf7a914c35
burritoslut <kingburrito@burritoslut.com>
2023-07-08 13:43:47 -0700

    Fixed config routing

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
 
-export const SERVER_URL = 'https://burritoserver-fcf9b552240d-a66d4f496f0b.herokuapp.com/';
+export const SERVER_URL = 'https://burritoserver-fcf9b552240d-a66d4f496f0b.herokuapp.com';
